6a925783c2684c119267223bef9af94555eadf5a
Aswin Kalarickal <aswinkalarickal@gmail.com>
2015-11-13 21:36:55 +0530

    0.0.2

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Galleria :: localhost</title>
 
     <link href='https://fonts.googleapis.com/css?family=Exo:300' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.9/themes/default/style.min.css" />
-
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.9/jstree.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 </head>
 <body>
 
@@ -18,37 +16,27 @@
         font-family: 'Exo', sans-serif;
         font-weight: 300;
         font-size: 16px;
-        margin: 0;
-        padding: 0;
     }
 
-    #breadcrumbs {
-        height: 50px;
-        line-height: 50px;
-        padding: 0px 10px;
-        width: 100%;
-        position: fixed;
-        background-color: black;
-        color: white;
-    }
+    @media (min-width: 992px) {
 
-    #folder {
-        width: 25%;
-        height: calc(100% - 50px);
-        top: 50px;
-        overflow: auto;
-        float: left;
-        position: fixed;
-        padding: 10px;
-    }
+        #breads {
+            position: fixed;
+            width: 100%;
+            z-index: 1000;
+        }
 
-    #gallery {
-        width: 75%;
-        margin-top: 50px;
-        min-height: 100px;
-        height: auto;
-        float: right;
-        padding: 10px;
+        #folder {
+            margin-top: 58px;
+            height: calc(100% - 58px);
+            position: fixed;
+            overflow-y: auto;
+        }
+
+        #gallery {
+            margin-top: 58px;
+        }
+
     }
 
     #gallery img {
@@ -58,84 +46,85 @@
     }
 
 </style>
-<div id="breadcrumbs">
-    <?php
-    error_reporting(0);
-    $path = $_GET['path'];
-    if($path == '') {
-        $path = ".";
-    }
-    echo $path
-    ?>
-</div>
 
-<div id="folder">
-    <?php
-    $root = '.';
-
-    function getDirContents($dir, $rootpath) {
-        echo '<ul>';
-        $files = scandir($dir);
-
-        foreach($files as $key => $value){
-            $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
-            $exclude_array = array(".", "..", ".idea", ".git");
-            if(is_dir($path) && !in_array($value, $exclude_array)) {
-                $filepath = $rootpath.DIRECTORY_SEPARATOR.$value;
-                echo '<li><a href="?path='.$filepath.'">'.$value.'</a>';
-                getDirContents($path, $filepath);
-                echo '</li>';
+<nav id="breads">
+    <ol id="" class="breadcrumb">
+        <?php
+        error_reporting(0);
+        $path = $_GET["path"];
+        if($path == "") {
+            $path = ".";
+        }
+        $crumbs = explode("/", $path);
+        $folder_path = array();
+        foreach($crumbs as $folder_name) {
+            if($folder_name == ".") {
+                $name = "localhost";
             } else {
+                $name = $folder_name;
+            }
+            array_push($folder_path, $folder_name);
+            echo '<li><a href="?path='.implode("/", $folder_path).'">'.$name.'</a></li>';
+        }
+        ?>
+    </ol>
+</nav>
 
+<div id="folder" class="col-md-3">
+    <ul class="list-group">
+        <?php
+        $path = $_GET["path"];
+        if($path == "") {
+            $path = ".";
+        }
+
+        function getDirContents($dir) {
+            $files = scandir($dir);
+            if($dir != ".") {
+                $crumbs = explode("/", $dir);
+                array_pop($crumbs);
+                echo '<li class="list-group-item"><a href="?path=' . implode("/", $crumbs) . '">Go up &uparrow;</a></li>';
+            }
+
+            foreach($files as $key => $value){
+                $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+                $exclude_array = array(".", "..", ".idea", ".git");
+                if(is_dir($path) && !in_array($value, $exclude_array)) {
+                    $filepath = $dir.DIRECTORY_SEPARATOR.$value;
+                    echo '<li class="list-group-item"><a href="?path='.$filepath.'">'.$value.'</a></li>';
+                }
             }
         }
-        echo '</ul>';
-    }
 
-    echo '<ul><li><a href="?path=.">localhost</a>';
-    getDirContents($root, '.');
-    echo '</li></ul>';
-    ?>
+        getDirContents($path);
+        ?>
+    </ul>
 </div>
 
-<div id="gallery">
+<div id="gallery" class="col-md-9 col-md-offset-3">
     <?php
     $dir = $_GET['path'];
     if($dir == '') {
         $dir = '.';
     }
 
-    $iter = scandir($dir);
+    $folders = scandir($dir);
 
     $index = 0;
-    foreach ($iter as $path) {
+    foreach ($folders as $path) {
         $extension = pathinfo($path, PATHINFO_EXTENSION);
         if(in_array($extension, array("jpg", "JPG", "jpeg", "JPEG", "png", "PNG"))) {
             $fullpath = $dir.'/'.$path;
-            echo '<a href="'.$fullpath.'"><img src="'.$fullpath.'"></a><br>';
+            echo '<a href="'.$fullpath.'"><img src="'.$fullpath.'"></a><br><br>';
             $index++;
         }
     }
 
     if($index == 0) {
-        echo 'Empty directory';
+        echo 'No items to view!';
     }
     ?>
 </div>
-
-<script>
-    $(document).ready(function() {
-        $("#folder").bind("loaded.jstree", function(event, data) {
-            data.instance.open_all();
-        });
-        $("#folder").jstree()
-            .bind("select_node.jstree", function (e, data) {
-                $('#folder').jstree('save_state');
-            }).on("activate_node.jstree", function(e,data){
-                window.location.href = data.node.a_attr.href;
-            });
-    });
-</script>
 
 </body>
 </html>
